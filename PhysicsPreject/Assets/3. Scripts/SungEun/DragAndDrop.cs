0767c4bc9c2c563ffb6ecde108bdedd97ab743d5
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2022-09-14 16:30:46 +0900

    배아파 흐흐ㅡㄱ

--- PhysicsPreject/Assets/3. Scripts/SungEun/DragAndDrop.cs
+++ PhysicsPreject/Assets/3. Scripts/SungEun/DragAndDrop.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+    // IBeginDragHendler는 드래그를 시작할 때 호출됨. IEndDragHandler는 마우스클릭을 때면 호출이됨, IDragHandler는 드래그 하고 있을 때 계속해서 호출됨.
 {
-    public static Vector2 defaultPos;
+    //public static Vector2 defaultPos;
 
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        defaultPos = this.transform.position;
+        //defaultPos = this.transform.position;
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-        Vector2 currentPos = eventData.position;
-        this.transform.position = currentPos;
+        //Vector2 currentPos = eventData.position;
+        //this.transform.position = currentPos;
+
+        transform.position = eventData.position;
     }
 
     void IEndDragHandler.OnEndDrag(PointerEventData eventData)
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        this.transform.position = defaultPos;
+        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //this.transform.position = defaultPos;
     }
 }
