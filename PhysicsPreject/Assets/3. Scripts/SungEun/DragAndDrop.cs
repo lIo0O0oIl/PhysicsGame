bca340cb051191896b2b515704970827b3ebbd1f
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2022-09-15 14:20:56 +0900

    DragDrop
    찐 구현?

--- PhysicsPreject/Assets/3. Scripts/SungEun/DragAndDrop.cs
+++ PhysicsPreject/Assets/3. Scripts/SungEun/DragAndDrop.cs
@@ -1,40 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
-    // IBeginDragHendler는 드래그를 시작할 때 호출됨. IEndDragHandler는 마우스클릭을 때면 호출이됨, IDragHandler는 드래그 하고 있을 때 계속해서 호출됨.
+public class DragAndDrop : MonoBehaviour
 {
-    //public static Vector2 defaultPos;
-    public float xMin, yMin, xMax, yMax;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    public Vector3 thisPos;
+    float startPosx;
+    float startPosY;
+    bool isHeld = false;
+    public bool isInLine;
+    float stickPosY;
 
-    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    private void Start()
     {
-        //defaultPos = this.transform.position;
+        thisPos = this.transform.position;
     }
-
-    void IDragHandler.OnDrag(PointerEventData eventData)
-    {
-        //Vector2 currentPos = eventData.position;
-        //this.transform.position = currentPos;
-
-        if (transform.position = new Vector3(1, 4, 5))
-
-        
-        transform.position = eventData.position;
-        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0);
-
-    }
-
-    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
-    {
-        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //this.transform.position = defaultPos;
-    }
-
     private void Update()
     {
-        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0);
+        if (isHeld)
+        {
+            Vector2 mousePos;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            this.gameObject.transform.position = new Vector2(mousePos.x - startPosx, mousePos.y - startPosY);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            spriteRenderer.color = new Color(1f, 1f, 1f, .5f);
+            Vector3 mousePos;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+            startPosx = mousePos.x - this.transform.position.x;
+            startPosY = mousePos.y - this.transform.position.y;
+
+
+            isHeld = true;
+
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+        isHeld = false;
+
+        if (isInLine)
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.localPosition.x, stickPosY, -1f);
+        else
+            this.gameObject.transform.position = thisPos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("stick"))
+        {
+            isInLine = true;
+            stickPosY = other.transform.position.y;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("stick"))
+        {
+            isInLine = false;
+        }
     }
 }
