efb48705feee61ba42821f9068f5ffe2f329b151
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2022-09-20 00:34:53 +0900

    중간완성

--- PhysicsPreject/Assets/3. Scripts/SungEun/DragAndDrop.cs
+++ PhysicsPreject/Assets/3. Scripts/SungEun/DragAndDrop.cs
@@ -14,10 +14,24 @@ public class DragAndDrop : MonoBehaviour
     float stickPosY;
     bool isGo = true;
 
+
+    public bool nnn = false;
+
+
+    Move move;
+
     private void Start()
     {
         thisPos = this.transform.position;
+
+        move = FindObjectOfType<Move>();
     }
+
+    public void StartN()
+    {
+        thisPos = this.transform.position;
+    }
+
     private void Update()
     {
         if (isHeld)
@@ -51,6 +65,20 @@ public class DragAndDrop : MonoBehaviour
         }
     }
 
+    public void NN(bool a)
+    {
+        if (a)
+        {
+            nnn = true;
+            thisPos = this.transform.position;
+        }
+        else
+        {
+            nnn = false;
+            thisPos = this.transform.position;
+        }
+    }
+
     private void OnMouseUp()
     {
         spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
@@ -59,7 +87,15 @@ public class DragAndDrop : MonoBehaviour
         if (isInLine || !isGo)
         {
             this.gameObject.transform.position = new Vector3(gameObject.transform.position.x, stickPosY, 0);
-            transform.DOMoveX(10f, 2f).SetEase(Ease.Linear);
+
+            if (nnn)
+            {
+                transform.DOMoveX(4.5f, 2f).SetEase(Ease.Linear);
+            }
+            else
+            {
+                transform.DOMoveX(10f, 2f).SetEase(Ease.Linear);
+            }
             Invoke("Del", 2f);
         }
         else
@@ -88,6 +124,15 @@ public class DragAndDrop : MonoBehaviour
     void Del()
     {
         //Destroy(gameObject);
+        move.gogo++;
         gameObject.SetActive(false);
+    }
+
+    public void PB()
+    {
+        gameObject.GetComponent<SpriteRenderer>().DOFade(0f, 1.5f);
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        gameObject.SetActive(true);
+        gameObject.transform.position = thisPos;
     }
 }
--- PhysicsPreject/Assets/3. Scripts/SungEun/Move.cs
+++ PhysicsPreject/Assets/3. Scripts/SungEun/Move.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Move : MonoBehaviour
 {
@@ -17,13 +18,34 @@ public class Move : MonoBehaviour
 
     SpriteRenderer[] p_1A, n_1A;
 
+    [SerializeField] GameObject backButton, backButton_n;
+    public int gogo = 0;
 
-    // Start is called before the first frame update
+
+    DragAndDrop dragAndDrop;
+
+
     void Start()
     {
         title.GetComponent<TextMeshProUGUI>().DOFade(1f, 2f);
         Invoke("Setting", 2.5f);
+
+        dragAndDrop = FindObjectOfType<DragAndDrop>();
     }
+
+    private void Update()
+    {
+        if (gogo == 7 && !dragAndDrop.nnn)
+        {
+            backButton.SetActive(true);
+        }
+        else if (gogo == 7 && dragAndDrop.nnn)
+        {
+            backButton_n.SetActive(true);
+        }
+    }
+
+
 
     void Setting()
     {
@@ -40,20 +62,33 @@ public class Move : MonoBehaviour
         p_B.SetActive(true);
         n_B.SetActive(true);
 
-        p_S.GetComponent<SpriteRenderer>().DOFade(1f, 1.5f); ;
+        p_S.GetComponent<SpriteRenderer>().DOFade(1f, 1.5f);
         n_S.GetComponent<SpriteRenderer>().DOFade(1f, 1.5f);
         P.GetComponent<TextMeshProUGUI>().DOFade(1f, 1.5f);
         N.GetComponent<TextMeshProUGUI>().DOFade(1f, 1.5f);
     }
 
+
+
     public void P_S()
     {
+        for (int i = 0; i < oO.Length; i++)
+        {
+            oO[i].transform.localScale = new Vector3(15, 15, 1);
+        }
+        oO[0].transform.position = new Vector3(0, -4, 0);
+        oO[1].transform.position = new Vector3(2.97f, -3.28f, 0);
+        oO[2].transform.position = new Vector3(5.23f, -4f, 0);
+        oO[3].transform.position = new Vector3(7.43f, -3.1f, 0);
+        oO[4].transform.position = new Vector3(-2.97f, -3.28f, 0);
+        oO[5].transform.position = new Vector3(-5.23f, -4f, 0);
+        oO[6].transform.position = new Vector3(-7.43f, -3.1f, 0);
+
         n_S.SetActive(false);
         P.SetActive(false);
         N.SetActive(false);
         p_B.SetActive(false);
         n_B.SetActive(false);
-
 
         p_S.transform.DOMove(new Vector3(-16.8f, 20.8f, 0),3f).SetEase(Ease.InQuad);
         p_S.transform.DOScale(new Vector3(210, 210, 0), 3f).SetEase(Ease.InQuad);
@@ -82,8 +117,22 @@ public class Move : MonoBehaviour
         }
     }
 
+
+
     public void N_S()
     {
+        for (int i = 0; i < oO.Length; i++)
+        {
+            oO[i].transform.localScale = new Vector3(5, 5, 1);
+        }
+        oO[0].transform.position = new Vector3(0, 1.4f, 0);
+        oO[1].transform.position = new Vector3(0.9f, 1.15f, 0);
+        oO[2].transform.position = new Vector3(1.87f, 1.21f, 0);
+        oO[3].transform.position = new Vector3(2.78f, 1.58f, 0);
+        oO[4].transform.position = new Vector3(-0.9f, 1.15f, 0);
+        oO[5].transform.position = new Vector3(-1.87f, 1.2f, 0);
+        oO[6].transform.position = new Vector3(-2.78f, 1.58f, 0);
+
         p_S.SetActive(false);
         P.SetActive(false);
         N.SetActive(false);
@@ -106,5 +155,104 @@ public class Move : MonoBehaviour
         {
             n_1A[i].DOFade(1f, 1.5f);
         }
+
+        Invoke("SetB_B", 1.2f);
+    }
+
+    void SetB_B()
+    {
+        for (int i = 0; i < oO.Length; i++)
+        {
+            oO[i].DOFade(1f, 1.5f);
+        }
+    }
+
+
+
+
+
+
+    public void GoNext(string Scenename)
+    {
+        SceneManager.LoadScene(Scenename);
+    }
+
+
+
+
+
+
+    public void Back_B_P()
+    {
+        gogo = 0;
+
+        backButton.SetActive(false);
+
+        for (int i = 0; i < p_1A.Length; i++)
+        {
+            p_1A[i].DOFade(0f, 1.5f);
+        }
+
+        p_S.transform.DOMove(new Vector3(-4f, 0, 0), 3f).SetEase(Ease.InQuad);
+        p_S.transform.DOScale(new Vector3(10, 10, 0), 3f).SetEase(Ease.InQuad);
+
+        n_S.SetActive(true);
+        P.SetActive(true);
+        N.SetActive(true);
+
+        n_S.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        P.GetComponent<TextMeshProUGUI>().color = new Color(0, 0, 0, 0);
+        N.GetComponent<TextMeshProUGUI>().color = new Color(0, 0, 0, 0);
+
+        Invoke("Back_1_2", 3f);
+    }
+
+    void Back_1_2()
+    {
+        p_B.SetActive(true);
+        n_B.SetActive(true);
+
+        n_S.GetComponent<SpriteRenderer>().DOFade(1f, 1.5f);
+        P.GetComponent<TextMeshProUGUI>().DOFade(1f, 1.5f);
+        N.GetComponent<TextMeshProUGUI>().DOFade(1f, 1.5f);
+    }
+
+
+    public void Back_B_N()
+    {
+        gogo = 0;
+
+        backButton_n.SetActive(false);
+
+        for (int i = 0; i < n_1A.Length; i++)
+        {
+            n_1A[i].DOFade(0f, 1.5f);
+        }
+
+        n_S.transform.DOMove(new Vector3(4, 0, 0), 3f).SetEase(Ease.InQuad);
+        n_S.transform.DOScale(new Vector3(10, 10, 0), 3f).SetEase(Ease.InQuad);
+        Camera.main.DOOrthoSize(5f, 3f);
+
+        p_S.SetActive(true);
+        P.SetActive(true);
+        N.SetActive(true);
+
+        p_S.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        P.GetComponent<TextMeshProUGUI>().color = new Color(0, 0, 0, 0);
+        N.GetComponent<TextMeshProUGUI>().color = new Color(0, 0, 0, 0);
+
+        Invoke("Back_2_2", 3f);
+
+        
+    }
+
+    void Back_2_2()
+    {
+        p_B.SetActive(true);
+        n_B.SetActive(true);
+
+        p_S.GetComponent<SpriteRenderer>().DOFade(1f, 1.5f);
+        P.GetComponent<TextMeshProUGUI>().DOFade(1f, 1.5f);
+        N.GetComponent<TextMeshProUGUI>().DOFade(1f, 1.5f);
     }
 }
