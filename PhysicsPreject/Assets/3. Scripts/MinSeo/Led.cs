c57324958eee1b498206c498f02bd94dadc9dcd5
minseo <cms20061112@gmail.com>
2022-09-19 23:26:34 +0900

    색 깜빡임

--- PhysicsPreject/Assets/3. Scripts/MinSeo/Led.cs
+++ PhysicsPreject/Assets/3. Scripts/MinSeo/Led.cs
@@ -8,18 +8,25 @@ public class Led : MonoBehaviour
     [SerializeField]
     private GameObject led;
 
-    SpriteRenderer[] ledF;
-    
-    private void Start()
+    private SpriteRenderer scolor;
+
+    [SerializeField]
+    private float sec = 0.5f;
+
+    private void Awake()
     {
-        Invoke("FadeIn", 1f);
+        scolor = led.GetComponent<SpriteRenderer>();
+
+        StartCoroutine(Blink());
+        Invoke("FadeIn", 3f);
     }
 
     private void FadeIn()
     {
         led.transform.DOMove(new Vector3(0, 7, 0), 3f);
         led.transform.DOScale(new Vector3(130, 130, 130), 3f);
-        Invoke("FadeOut", 1f);
+        Invoke("FadeOut", 2f);
+        led.SetActive(false);
     }
 
     private void FadeOut()
@@ -27,5 +34,19 @@ public class Led : MonoBehaviour
         led.SetActive(true);
         led.transform.DOMove(new Vector3(0, 1, 0), 3f);
         led.transform.DOScale(new Vector3(12, 12, 12), 3f);
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            scolor.color = Color.red;
+
+            yield return new WaitForSeconds(0.5f);
+
+            scolor.color = Color.white;
+
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
