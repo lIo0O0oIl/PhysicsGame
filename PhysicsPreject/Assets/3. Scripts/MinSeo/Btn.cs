38853f734ae0dc821a4a95a19e24766a59bcf3d6
tjfdk <tjfdk6201@gmail.com>
2022-09-21 00:01:38 +0900

    ewrwr
    werwer

--- PhysicsPreject/Assets/3. Scripts/MinSeo/Btn.cs
+++ PhysicsPreject/Assets/3. Scripts/MinSeo/Btn.cs
@@ -1,6 +1,9 @@
+using System.Xml.Serialization;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Btn : MonoBehaviour
 {
@@ -10,11 +13,13 @@ public class Btn : MonoBehaviour
     private GameObject p_Semiconductor;
     [SerializeField]
     private GameObject n_Semiconductor;
+    [SerializeField] private Canvas can = null;
 
     public void ClickP()
     {
         first.SetActive(false);
         p_Semiconductor.SetActive(true);
+        can.gameObject.SetActive(false);
     }
 
     public void ClickN()
